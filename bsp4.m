0da2e0a3560696aeb8e64ff78e19eb9af0ae7b73
Chris <christian.lascsak7@gmail.com>
2018-04-12 19:43:54 +0200

    bsp 4 added

---
+++ bsp4.m
@@ -0,0 +1,23 @@
+Im424a=imread('424a.png');
+Im429a=imread('429a.png');
+
+Im424a=fft2(Im424a);
+Im424a=fftshift(Im424a);
+#combine real an imaginary part
+Im424a=abs(Im424a);
+#reduce contrast (just for imshow() call
+#Im424a=log(1+Im424a);
+
+imagesc(Im424a,clim=[5 13]);
+#imshow(Im424a,[]);
+
+Im429a=fft2(Im429a);
+Im429a=fftshift(Im429a);
+
+#reduce contrast & show imaginary and real part
+figure,imagesc(log(1+abs(Im429a)),clim=[5, 13]);
+
+
+
+
+
