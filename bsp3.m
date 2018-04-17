d33226c95b8213d5432e39bb419d797b971a9056
Chris <christian.lascsak7@gmail.com>
2018-04-17 16:59:48 +0200

    smoothing added

--- bsp3.m
+++ bsp3.m
@@ -9,10 +9,12 @@ laplacian = conv2(original, lap, "same");
 
 figure,imshow(laplacian, []);
 title("Laplace");
+
 #sharpening
 sharpened = original + laplacian;
 figure,imshow(sharpened, []);
 title("Laplace sharpened");
+
 #Sobel
 sx = [1,0,-1;2,0,-2;1,0,-1];
 sy= [1,2,1; 0,0,0;-1,-2,-1];
@@ -27,3 +29,20 @@ sobel += original;
 
 figure,imshow(sobel, []);
 title("Sobel sharpened");
+
+#smoothing 5x5
+smooth(1:5,1:5) = 1;
+smooth *= 1/25;
+smoothed = conv2(sobel, smooth, 'same');
+
+figure,imshow(smoothed, []);
+title("Smoothed 5x5");
+
+#mask image
+mask= conv2(sharpened,smoothed,'same');
+figure,imshow(mask, []);
+title("Mask");
+
+
+
+
