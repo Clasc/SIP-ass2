5cf430fd33a8f292ff7300bd004b9bf35d56639f
Chris <christian.lascsak7@gmail.com>
2018-04-17 16:25:05 +0200

    titles added

--- bsp3.m
+++ bsp3.m
@@ -1,24 +1,29 @@
 original = imread('343a.tif');
 
 figure,imshow(original,[]);
+title("Original");
 lap = [0,1,0;1,-4,1;0,1,0] * -1; #sieht schaerfer aus
 
 #Laplace filter
 laplacian = conv2(original, lap, "same");
 
 figure,imshow(laplacian, []);
-
+title("Laplace");
 #sharpening
 sharpened = original + laplacian;
 figure,imshow(sharpened, []);
-
+title("Laplace sharpened");
 #Sobel
 sx = [1,0,-1;2,0,-2;1,0,-1];
 sy= [1,2,1; 0,0,0;-1,-2,-1];
 
 
-gx = conv2(original, sx);
-gy = conv2(original, sy);
+gx = conv2(original, sx, 'same');
+gy = conv2(original, sy, 'same');
 
-sobel = sqrt(pow2(gx)+pow2(gy));
+sobel = gx+gy;
+sobel += original;
+
+
 figure,imshow(sobel, []);
+title("Sobel sharpened");
