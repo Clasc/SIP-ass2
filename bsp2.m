da771ff2249dfb344eaa86a0ed4ad93273f1dc4d
Chris <christian.lascsak7@gmail.com>
2018-04-21 15:43:09 +0200

    deconv parameters switched

--- bsp2.m
+++ bsp2.m
@@ -4,14 +4,14 @@
 close all;
 clear all;
 
-x = [0,1,2,1,0,0];
-h = [0,0,0,-1,,-2,0,2,1,0,0];
-impresponse = deconv(x,h);
-x1 = [0,0,1,2,3,4,3,2,1,0,0];
+x = [1,2,1];
+h = [-1,-2,0,2,1];
+impresponse = deconv(h,x);
+x1 = [1,2,3,4,3,2,1];
 h2 = conv1(x1,impresponse);
 
-subplot(1,2,1),stem(impresponse)
-title = ("impulse response");
+figure,subplot(1,2,1),stem(impresponse);
+title("impulse response");
 
-subplot(1,2,1),stem(h2);
-title = ("response to x1");
+subplot(1,2,2),stem(h2);
+title("response to x1");
