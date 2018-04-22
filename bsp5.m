3e54e3a8db445fe995869bf25bf36196a7f8954e
Chris <christian.lascsak7@gmail.com>
2018-04-22 10:58:06 +0200

    u, v bug fixed

--- bsp5.m
+++ bsp5.m
@@ -4,6 +4,8 @@
 
 close all;
 clear all;
+
+i=1;
 img = imread("429a.png");
 [m,n,z] = size(img);
 D0 = 0.03; #konstante size für gaussian filter
@@ -14,12 +16,36 @@ indexu = find(u > m/2);
 u(indexu) = u(indexu) - m; 
  #u = u.- m/2;
 indexv = find(v > n/2);
-v(indexu) = v(indexu) - n; 
+v(indexv) = v(indexv) - n;  #flip indices around the axes
  #v = v.- v/2;
 [V, U]= meshgrid(v, u);
 
+F = fft2(img);
+F2 = abs(F);
+F2 = log(1+F2);
+F2 = fftshift(F2);
+
+subplot(2,2,i++),imshow(F2, []);
+title("Fourier");
+
+D = sqrt(U.^2 + V.^2);
+
+H = exp((-1)*(D.^2)/2*D0^2); #create lowpassfilter
+
+blurred = H.* F2; 
+
+usmask = F2 - blurred; #unsharpening mask created
+
+sharpened = F2 + usmask; 
+
+sharpened = real(ifft2(sharpened)); #real part of inverse FFT
+sharpened = log(1+sharpened); 
 
 
+subplot(2,2,i++),imshow(usmask, []);
+title("Unsharpening mask");
 
-D;
-H; # H-> gaussian filter -> .* original  = blurred
+subplot(2,2,i++),imshow(img, []);
+title("original");
+subplot(2,2,i++),imshow(sharpened, []);
+title("Sharpened by unsharpening mask");
