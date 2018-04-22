774e7513b2ccb0cdc05301646a42c4cdcdf316d9
Chris <christian.lascsak7@gmail.com>
2018-04-22 17:56:37 +0200

    report added

--- bsp4.m
+++ bsp4.m
@@ -12,7 +12,7 @@ Im424a=fft2(Im424a);
 Im424a=fftshift(Im424a);
 #combine real an imaginary part
 Im424a=abs(Im424a);
-#reduce contrast (just for imshow() call
+#reduce contrast (just for imshow() call)
 #Im424a=log(1+Im424a);
 figure,imagesc(Im424a,clim=[5 13]);
 #imshow(Im424a,[]);
